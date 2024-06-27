ed0798e1f6388093f353091ff1c2bcb689a08c1a
vaishnavi <vasanjay@deloitte.com>
2024-06-28 00:13:23 +0530

    Basics

---
+++ If let Guard let/If let Guard let/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  If let Guard let
+//
+//  Created by Sanjay, Vaishnavi on 28/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
