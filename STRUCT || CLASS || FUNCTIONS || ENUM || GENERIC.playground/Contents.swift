c9c5e67869e1d5e0172f18c7eac89d7176147626
vaishnavi <vasanjay@deloitte.com>
2024-07-08 22:34:09 +0530

    structure

--- STRUCT || CLASS || FUNCTIONS || ENUM || GENERIC.playground/Contents.swift
+++ STRUCT || CLASS || FUNCTIONS || ENUM || GENERIC.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Struct and Class Difference
+struct MyStruct {
+    var name :String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+func forStruct() {
+    var fName = MyStruct(name: "Vaishnavi")
+    var LName = fName
+    LName.name = "Sonu"
+    print(fName.name)
+    print(LName.name)
+}
+
+//Enum/
+//Two types
+//1] Without type
+
